f81c21a41bfc601a2eba6b17ab378c43ef215eeb
sirisha-fulara <166629183+sirisha-fulara@users.noreply.github.com>
2025-07-14 21:29:29 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
