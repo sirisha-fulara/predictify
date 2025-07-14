7c67775895f46933a581b68451e4cf40288618a7
sirisha-fulara <166629183+sirisha-fulara@users.noreply.github.com>
2025-07-14 22:23:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI= "sqlite:///login_db.sqlite"
     SQLALCHEMY_TRACK_MODIFICATIONS= False
     JWT_TOKEN_LOCATION= ["cookies"]
-    JWT_COOKIE_SECURE= False
+    JWT_COOKIE_SECURE= True
     JWT_ACCESS_COOKIE_PATH= '/'
-    JWT_COOKIE_CSRF_PROTECT= False
+    JWT_COOKIE_CSRF_PROTECT= True
