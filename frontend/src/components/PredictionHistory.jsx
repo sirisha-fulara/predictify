e585994df255a17cfdb238579054a21aadb80d7c
sirisha-fulara <166629183+sirisha-fulara@users.noreply.github.com>
2025-07-14 21:33:20 +0530

    Update PredictionHistory.jsx

--- frontend/src/components/PredictionHistory.jsx
+++ frontend/src/components/PredictionHistory.jsx
@@ -1,5 +1,5 @@
 // PredictionHistory.jsx
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function PredictionHistory() {
