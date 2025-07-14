6581dac222bc5747b0a9414947a02bbe729e7ef4
sirisha-fulara <166629183+sirisha-fulara@users.noreply.github.com>
2025-07-14 15:22:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
