7e613b271f0ec1a1bfdcc0dd60cc1f7fc333c2cf
sirisha-fulara <sirishaf2005@gmail.com>
2026-01-19 13:21:09 +0530

    Fix: Remove unused imports in App.js (ESLint)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import Dashboard from "./components/Dashboard";
 import Register from './components/Register';
